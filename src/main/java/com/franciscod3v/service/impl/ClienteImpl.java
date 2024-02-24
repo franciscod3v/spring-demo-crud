7c4240c86c182b9c94d44f7d3d2c234761ba94fd
Franciscod3v <franciscod3v@gmail.com>
2024-02-23 23:16:07 -0500

    ClienteImpl finished

--- src/main/java/com/franciscod3v/service/impl/ClienteImpl.java
+++ src/main/java/com/franciscod3v/service/impl/ClienteImpl.java
@@ -1,4 +1,29 @@
 package com.franciscod3v.service.impl;
 
-public class ClienteImpl {
+import com.franciscod3v.model.entity.Cliente;
+import com.franciscod3v.service.ICliente;
+import com.franciscod3v.model.dao.ClienteDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteImpl implements ICliente {
+
+    @Autowired
+    private ClienteDao clienteDao;
+
+    @Override
+    public Cliente save(Cliente cliente) {
+        return clienteDao.save(cliente);
+    }
+
+    @Override
+    public Cliente findbyId(Integer id) {
+        return clienteDao.findById(id).orElse(null);
+    }
+
+    @Override
+    public void delete(Cliente cliente) {
+        clienteDao.delete(cliente);
+    }
 }
